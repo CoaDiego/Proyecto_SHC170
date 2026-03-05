d66b2f3912ae64c4806ca99631820ffc890c0ae0
Luis Alberto Ibarra Calderon <ibarra.luisalberto23@gmail.com>
2026-03-05 09:34:57 -0400

    mat251

--- excel-app/src/components/MAT251/MAT251.jsx
+++ excel-app/src/components/MAT251/MAT251.jsx
@@ -2,7 +2,7 @@ export default function MAT251() {
   return (
     <div>
       <h2>MAT251 - Estadística Matemática</h2>
-      <p>En construcción... próximamente cálculos avanzados. hola</p>
+      <p>En construcción... próximamente cálculos avanzados.</p>
     </div>
   );
 }
