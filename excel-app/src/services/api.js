80dbb83241a246940eaf88b9df592b0dcea9eac1
CoaDiego <74245398+CoaDiego@users.noreply.github.com>
2026-06-01 22:05:55 -0400

    Update BASE_URL to use only environment variable
    Remove default fallback for BASE_URL in api.js

--- excel-app/src/services/api.js
+++ excel-app/src/services/api.js
@@ -4,7 +4,7 @@
 
 /*uvicorn main:app --reload*/
 
-export const BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+export const BASE_URL = import.meta.env.VITE_API_URL;
 
 
 export const api = {
